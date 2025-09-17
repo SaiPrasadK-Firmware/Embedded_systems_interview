7d94371da4cd2dd58d79de31e28d637cf245f101
Sai <sp0404@outlook.in>
2025-09-17 17:29:32 +0530

    Update Q43_Endianness_test.c

---
+++ Questions/3_Bitwise_Tricks/Q43_Endianness_test.c
@@ -0,0 +1,18 @@
+ // Endians
+#include <stdio.h>
+#include <stdint.h>
+
+
+int main()
+{
+   int num =1;
+   char *ptr = (char*)&num;
+   if(*ptr==1)
+   {
+       printf("Little Endian\n");
+   }else{
+       printf("Big Endian\n");
+   }
+  
+    return 0;
+}
